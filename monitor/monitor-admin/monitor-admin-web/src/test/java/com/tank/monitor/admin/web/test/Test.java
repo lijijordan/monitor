430464451ca59b355ea523c5f2d38b4b0dc56c74
liji_jordan <liji.jordan@gmail.com>
2016-05-04 16:04:45 +0800

    update

---
+++ monitor/monitor-admin/monitor-admin-web/src/test/java/com/tank/monitor/admin/web/test/Test.java
@@ -0,0 +1,15 @@
+package com.tank.monitor.admin.web.test;
+
+import junit.framework.TestCase;
+
+import org.springframework.util.Assert;
+
+public class Test extends TestCase{
+
+	@org.junit.Test
+	public void test(){
+		System.out.println("Run My test !");
+		Assert.isTrue(true);
+	}
+
+}
