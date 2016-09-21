eda960dc8f473dd2637410e9ca3091b95f830f62
zjy <zhaojinyi@xiaojiuwo.cn>
2016-09-21 14:57:54 +0800

    接收mqtt入库

--- monitor/monitor-device/monitor-device-web/src/main/java/com/monitor/device/web/service/MessageReceiverService.java
+++ monitor/monitor-device/monitor-device-web/src/main/java/com/monitor/device/web/service/MessageReceiverService.java
@@ -1,5 +1,12 @@
 package com.monitor.device.web.service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.monitor.common.vo.DataPointsVo;
+
 public class MessageReceiverService {
 
 	public MessageReceiverService() {
@@ -7,6 +14,20 @@ public class MessageReceiverService {
 	}
 
 	public void startCase(String message) {
-		// System.out.println(message);
+		System.out.println(message);
+		String msg = message;
+		RestTemplate restTemplate = new RestTemplate();
+		DataPointsVo vo = new DataPointsVo();
+		vo.setDataType(2);
+		vo.setDeviceSn("fisher01");
+		vo.setValue("35");
+		SimpleDateFormat sdf = new SimpleDateFormat(
+				"yyyy-MM-dd'T'HH:mm:ss.sss'Z'");
+		Date dtNow = sdf.getCalendar().getTime();
+		vo.setCollectTime(dtNow);
+		String res = restTemplate.postForObject(
+				"http://106.185.35.79:9099/monitor-device-web/dataPoints/add",
+				vo, String.class);
+		System.out.println(res);
 	}
 }
