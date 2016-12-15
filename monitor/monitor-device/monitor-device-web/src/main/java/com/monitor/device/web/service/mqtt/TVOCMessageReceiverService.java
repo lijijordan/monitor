4a21f343f79bfaf096293ecdbf45d83dc7136b12
lijiau <li.ji@mydreamplus.com>
2016-12-15 19:20:59 +0800

    TVOC MessagerReceiver

---
+++ monitor/monitor-device/monitor-device-web/src/main/java/com/monitor/device/web/service/mqtt/TVOCMessageReceiverService.java
@@ -0,0 +1,9 @@
+package com.monitor.device.web.service.mqtt;
+
+public class TVOCMessageReceiverService extends MessageReceiverService {
+
+	public TVOCMessageReceiverService() {
+		setDataType(9);
+	}
+
+}
