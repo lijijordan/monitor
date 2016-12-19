851a3532324880ee2052178c27dd50f07a52072f
yinhong2016 <yinhong2015@outlook.com>
2016-12-19 21:30:19 +0800

    提交代码

--- monitor/monitor-server/monitor-server-web/src/main/java/com/monitor/server/SwaggerConfig.java
+++ monitor/monitor-server/monitor-server-web/src/main/java/com/monitor/server/SwaggerConfig.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.joda.time.LocalDate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import com.monitor.server.comm.MessageUtil;
@@ -47,6 +48,15 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     }
 
     return docket;
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 
 }
